606f1acc4733adae9f8c664c07b6fa17d577ccbd
Johannes Møgster <johannes.mogster@mojotech.no>
2021-04-26 12:23:03 +0200

    framework work
    based on manual and ICRA18 example from FE https://frankaemika.github.io/docs/franka_ros.html#writing-your-own-controller https://github.com/frankaemika/icra18-fci-tutorial

---
+++ include/ros_panda/joint_position_control.h
@@ -0,0 +1,29 @@
+#pragma once
+
+#include <array>
+#include <string>
+#include <vector>
+
+#include <controller_interface/multi_interface_controller.h>
+#include <hardware_interface/joint_command_interface.h>
+#include <hardware_interface/robot_hw.h>
+#include <ros/node_handle.h>
+#include <ros/time.h>
+
+namespace panda_controllers {
+
+class JointPositionController : public controller_interface::MultiInterfaceController<
+                                           hardware_interface::PositionJointInterface> {
+ public:
+  bool init(hardware_interface::RobotHW* robot_hardware, ros::NodeHandle& node_handle) override;
+  void starting(const ros::Time&) override;
+  void update(const ros::Time&, const ros::Duration& period) override;
+
+ private:
+  hardware_interface::PositionJointInterface* position_joint_interface_;
+  std::vector<hardware_interface::JointHandle> position_joint_handles_;
+  ros::Duration elapsed_time_; // replace/remove this
+  std::array<double, 7> initial_pose_{};
+};
+
+}  // namespace panda_controllers
